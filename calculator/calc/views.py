e62c9981d40b7d42ba4490c9a387a8627a59d41e
roshan <roshan22@gmail.com>
2015-05-26 08:32:54 +0530

    first commit

---
+++ calculator/calc/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home_view(request):
+	d={"key":"hello world ",}
+	return render(request,'home.html',d)
+	   
+	
